666d78aa807fd92290d0f78f2f44989045e7ba67
desbog7 <desbog7@singularcom.local>
2017-04-11 16:08:37 -0500

    Functionalities - customBootstrap - js scrollHorizontal

--- src/scripts/javaScript/_functionalities.js
+++ src/scripts/javaScript/_functionalities.js
@@ -8,3 +8,10 @@ function linkAnimate () {
 	});
 }
 
+/*hacer scroll horizontal con btn rueda mouse solo en div class container-horizontal*/
+function scrollHorizontal () {
+	$('.container-horizontal').on('mousewheel', function(event, delta) {
+		this.scrollLeft -= (delta * 30);
+		event.preventDefault();
+	});
+}
