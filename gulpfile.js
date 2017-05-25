b13aa774b94ee2d9ce1fdd668d47aa725516747f
desbog7 <desbog7@DESBOG7>
2017-05-25 16:14:30 -0500

    ajustes metodologia Bem oocss smacss

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,11 @@ var gulp = require('gulp'),
 	mainBowerFiles = require('main-bower-files'),
 	wiredep = require('wiredep').stream;
 
-
+/***************************************** 
+	browser-sync 
+	- Levanta servidor local.
+	- Tarea $ gulp browser-sync.
+******************************************/
 gulp.task('browser-sync', function() {
 	browserSync({
 		server: {
@@ -27,18 +31,39 @@ gulp.task('browser-sync', function() {
 	});
 });
 
+/***************************************** 
+	Reload 
+	- Refresca el navegador automaticamente al ver cambios.
+	- Tarea $ gulp bs-reload.
+******************************************/
 gulp.task('bs-reload', function () {
 	browserSync.reload();
 });
 
-// images
+
+/***************************************** 
+	Images 
+	- optimizar imágenes (peso).
+	- Tarea $ gulp images.
+	- Directorio a precesar src/images.
+	- Directorio procesado dist/images.
+******************************************/
 gulp.task('images', function(){
 	gulp.src('src/images/**/*')
 		.pipe(cache(imagemin({ optimizationLevel: 9, progressive: true, interlaced: true })))
 		.pipe(gulp.dest('dist/images/'))
 });
 
-//styles
+/***************************************** 
+	Styles
+	- procesa archivos SASS.
+	- Tarea $ gulp styles.
+	- Archivo a precesar src/styles/*.scss.
+	- Archivo a procesado dist/styles/*.scss.
+	- Autoprefixer.
+	- Archivo comprimido.
+	- Mapa Archivo SCSS.
+******************************************/
 gulp.task('styles', function(){
 	gulp.src(['src/styles/**/*.scss'])
 		.pipe(plumber({
@@ -58,15 +83,26 @@ gulp.task('styles', function(){
 		.pipe(browserSync.reload({stream:true}))
 });
 
-//componentsCss
-gulp.task('componentsCss', function(){
-	gulp.src(['src/components/styles/**/*.scss'])
+/***************************************** 
+	Mix styles
+	- procesa archivos SASS.
+	- Tarea $ gulp mixCss.
+	- Importa archivos bower css - scss.
+	- Librerías externas 
+	- Archivo a precesar src/mix/styles/*.scss.
+	- Archivo a procesado dist/mix/styles/*.scss.
+	- Autoprefixer.
+	- Archivo comprimido.
+	- Mapa Archivo SCSS.
+******************************************/
+gulp.task('mixCss', function(){
+	gulp.src(['src/mix/styles/**/*.scss'])
 		.pipe(plumber({
 		errorHandler: function (error) {
 			console.log(error.message);
 			this.emit('end');
 		}}))
-		.pipe(concat('componentsMain.css'))
+		.pipe(concat('mixMain.css'))
 		.pipe(sourcemaps.init())
 		.pipe(sass())
 		.pipe(autoprefixer({ browsers: ["> 0%"] }))
@@ -74,11 +110,21 @@ gulp.task('componentsCss', function(){
 		.pipe(rename({suffix: '.min'}))
 		.pipe(minifycss())
 		.pipe(sourcemaps.write('/'))
-		.pipe(gulp.dest('dist/components/styles/'))
+		.pipe(gulp.dest('dist/mix/styles/'))
 		.pipe(browserSync.reload({stream:true}))
 });
 
-//typeScript
+/***************************************** 
+	TypeScript
+	- procesa archivos ts.
+	- Tarea $ gulp tipeScript.
+	- Compila archivos TypeScript a JS.
+	- Minifica archivos TypeScript.
+	- Une archivos TypeScript.
+	- Archivo a precesar src/scripts/tipeScript/*.ts.
+	- Archivo a procesado dist/scripts/tipeScript/*.js.
+	- Mapa Archivo js.
+******************************************/
 gulp.task('tipeScript', function () {
 	return gulp.src('src/scripts/tipeScript/**/*.ts')
 		.pipe(ts({
@@ -94,7 +140,17 @@ gulp.task('tipeScript', function () {
 		.pipe(browserSync.reload({stream:true}))
 });
 
-//scripts
+/***************************************** 
+	Scripts
+	- procesa archivos ts.
+	- Tarea $ gulp scripts.
+	- Compila archivos JS.
+	- Minifica archivos Js.
+	- Une archivos Js.
+	- Archivo a precesar src/scripts/javaScript/*.js.
+	- Archivo a procesado dist/scripts/javaScript/*.js.
+	- Mapa Archivo js.
+******************************************/
 gulp.task('scripts', function(){
 	return gulp.src('src/scripts/javaScript/**/*.js')
 		.pipe(plumber({
@@ -111,30 +167,51 @@ gulp.task('scripts', function(){
 		.pipe(browserSync.reload({stream:true}))
 });
 
-//components Js
+/***************************************** 
+	mix Js
+	- procesa archivos ts.
+	- Tarea $ gulp componentsJs.
+	- Compila archivos JS.
+	- Minifica archivos Js.
+	- Une archivos Js.
+	- Librerías externas (scripts de terceros). 
+	- Archivo a precesar src/mix/js/*.js.
+	- Archivo a procesado dist/mix/js/*.js.
+	- Mapa Archivo js.
+******************************************/
 gulp.task('componentsJs', function(){
-	return gulp.src('src/components/js/**/*.js')
+	return gulp.src('src/mix/js/**/*.js')
 		.pipe(plumber({
 		errorHandler: function (error) {
 			console.log(error.message);
 			this.emit('end');
 		}}))
-		.pipe(concat('components.js'))
+		.pipe(concat('mix.js'))
 		.pipe(babel())
-		.pipe(gulp.dest('dist/components/js/'))
+		.pipe(gulp.dest('dist/mix/js/'))
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
-		.pipe(gulp.dest('dist/components/js/'))
+		.pipe(gulp.dest('dist/mix/js/'))
 		.pipe(browserSync.reload({stream:true}))
 });
 
-//limpiar dis
+/***************************************** 
+	limpiar dis
+	- Eliminar todo directorio y archivo de dist/
+	- Tarea $ gulp clean.
+******************************************/
 //gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
-//funciones
+/***************************************** 
+	server
+	- Ejecuta las tareas: server, styles, mixCss, tipeScript, scripts, componentsJs, images, bs-reload
+	- Tarea $ gulp server.
+	- Levanta servidor local.
+	- Queda atento a cambios de todas las tareas.
+******************************************/
 gulp.task('server', ['browser-sync'], function(){
 	gulp.watch("src/styles/**/*.scss", ['styles']);
-	gulp.watch("src/components/styles/**/*.scss", ['componentsCss']);
+	gulp.watch("src/components/styles/**/*.scss", ['mixCss']);
 	gulp.watch("src/scripts/tipeScript/**/*.ts", ['tipeScript']);
 	gulp.watch("src/scripts/**/*.js", ['scripts']);
 	gulp.watch("src/components/js/**/*.js", ['componentsJs']);
@@ -142,12 +219,26 @@ gulp.task('server', ['browser-sync'], function(){
 	gulp.watch("*.html", ['bs-reload']);
 });
 
-//build
-gulp.task('build', ['images', 'styles', 'componentsCss', 'tipeScript', 'scripts', 'componentsJs'], function(){ });
+/***************************************** 
+	build
+	- Ejecuta las tareas: server, styles, mixCss, tipeScript, scripts, componentsJs, images, bs-reload.
+	- Reconstruir los archivos generados por las tareas.
+	- Tarea $ gulp build.
+******************************************/
+gulp.task('build', ['images', 'styles', 'mixCss', 'tipeScript', 'scripts', 'componentsJs'], function(){ });
 
-// bower
 
-// Crea directorios con los componentes Bower
+
+//=======================================================================//
+//******************************** bower ********************************//
+//=======================================================================//
+
+/***************************************** 
+	Bower - Incorporación manual.
+	- Busca: js, css y font para generar uno solo Archivo.
+	- Tarea $ gulp bower-cr.
+	- Tarea por Ajustar.
+******************************************/
 var dest_path =  'www';// Define paths variables
 gulp.task('bower-cr', function() {
 	var jsFilter = gulpFilter('***/**/*.js', {restore: true}),
@@ -177,6 +268,13 @@ gulp.task('bower-cr', function() {
 		.pipe(flatten())
 		.pipe(gulp.dest(dest_path + '/fonts'));
 });
+
+/***************************************** 
+	Bower Js - Incorporación manual.
+	- Busca: js, css y font para generar uno solo Archivo.
+	- Tarea $ gulp bower-cr.
+	- Tarea por Ajustar.
+******************************************/
 gulp.task('bowerJs-cr', function() {
 	
 	var dest_path = 'dist/components';// Define paths variables
@@ -195,7 +293,11 @@ gulp.task('bowerJs-cr', function() {
 
 });
 
-//inserta componentes Bower en markup
+/***************************************** 
+	Bower Css - Indexacion automatica a Markup.
+	- Busca: css para indexar a html-php por medio de etiquetas inteligentes.
+	- Tarea $ gulp bowerCss.
+******************************************/
 gulp.task('bowerCss', function () {
 	gulp.src('./view/components/head.php')
 		.pipe(wiredep({
@@ -204,6 +306,12 @@ gulp.task('bowerCss', function () {
 	}))
 		.pipe(gulp.dest('./view/components/'));
 });
+
+/***************************************** 
+	Bower Js - Indexacion automatica a Markup.
+	- Busca: Js para indexar a html-php por medio de etiquetas inteligentes.
+	- Tarea $ gulp bowerjs.
+******************************************/
 gulp.task('bowerjs', function () {
 	gulp.src('./view/components/scripts.php')
 		.pipe(wiredep({
@@ -212,12 +320,23 @@ gulp.task('bowerjs', function () {
 	}))
 		.pipe(gulp.dest('./view/components/'));
 });
-gulp.task('bower', ['bowerjs', 'bowerCss'], function(){ });
 
-// Defaul
+/***************************************** 
+	Bower (Bower Js - Bower Css)
+	- Ejecuta las tareas: bowerjs, bowerCss.
+	- Tarea $ gulp bowerIndexes.
+******************************************/
+gulp.task('bowerIndexes', ['bowerjs', 'bowerCss'], function(){ });
+
+/***************************************** 
+	Defaul Tarea
+	- Ejecuta las tareas: styles, mixCss, tipeScript, scripts, componentsJs, images.
+	- Tarea $ gulp.
+	- Queda atento a cambios de todas las tareas.
+******************************************/
 gulp.task('default', function(){
 	gulp.watch("src/styles/**/*.scss", ['styles']);
-	gulp.watch("src/components/styles/**/*.scss", ['componentsCss']);
+	gulp.watch("src/components/styles/**/*.scss", ['mixCss']);
 	gulp.watch("src/scripts/tipeScript/**/*.ts", ['tipeScript']);
 	gulp.watch("src/scripts/**/*.js", ['scripts']);
 	gulp.watch("src/components/js/**/*.js", ['componentsJs']);
--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 	<!--head-->
-	<?php include 'view/components/head.php';?>
+	<?php include 'view/modules/head.php';?>
 	<body>
 
 		<!--header-->
-		<?php include 'view/components/header.php';?>
+		<?php include 'view/modules/header.php';?>
 		<!--footer-->
-		<?php include 'view/components/footer.php';?>
+		<?php include 'view/modules/footer.php';?>
 		<!--scripts-->
-		<?php include 'view/components/scripts.php';?>
+		<?php include 'view/modules/scripts.php';?>
 	</body>
 </html>
