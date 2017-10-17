9326cc86b76eee34c361b24deec383d8d26b5a1d
desbog7 <desbog7@DESBOG7>
2017-10-17 14:37:37 -0500

    sapce

--- src/scripts/javaScript/_matchMedia.js
+++ src/scripts/javaScript/_matchMedia.js
@@ -92,3 +92,23 @@ function mediaQueryEsJs() {
 window.addEventListener("resize", () => {
   //console.log('Hola Resize');
 });
+
+
+/**
+ *  nav fixed
+ *    clase activa para efecto "menu-fixed"
+ *    menui => contenedor fixet
+ */
+function fixetNav () {
+  setTimeout(function () {
+    var menui = $('.upselling__nav');
+    var alturaMenu = menui.offset().top;
+    $(window).on('scroll', function () {
+      if ($(window).scrollTop() > alturaMenu) {
+        menui.addClass('menu-fixed');
+      } else {
+        menui.removeClass('menu-fixed');
+      }
+    });
+  }, 1000);
+}
