48a648a2aeb6aba35ba0bf3616184f3b0dde9b18
desbog7 <desbog7@DESBOG7>
2017-11-14 18:17:51 -0500

    ajustes js 2

---
+++ src/scripts/javaScript/functionalities/infoBrowser/_info-browser-called.js
@@ -0,0 +1,23 @@
+/**
+ *  Llamados
+ *
+ ==============================================================================*/
+window.addEventListener("resize", () => {
+  //detectBrowser();
+  //mobileDetecting();
+  //viewportSize();
+});
+
+
+/* =============================================================================*/
+
+(() => {
+  //detectBrowser();
+  // mobileDetecting();
+  //viewportSize();
+
+  if ($('body').is('.mobile')) {
+
+  }
+
+})();
