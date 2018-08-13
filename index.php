f7dbec3aeac9e81187400eaaf1c124dd4adccc3e
desbog7 <desbog7@DESBOG7>
2018-08-13 15:12:45 -0500

    theme

--- index.php
+++ index.php
@@ -2,13 +2,26 @@
 <html>
 	<!--head-->
 	<?php include 'view/modules/head.php';?>
-	<body>
+	<body class="theme-info">
 		<!--header-->
 		<?php include 'view/modules/header.php';?>
 		<!--footer-->
 		<?php include 'view/modules/footer.php';?>
 		<!--scripts-->
 		<?php include 'view/modules/scripts.php';?>
+		<div class="theme-light">
+			<div class="app-container">
+			<h1 class="title">Light theme</h1>
+			<button class="button">A button</button>
+			</div>
+		</div>
+
+		<div class="theme-dark">
+			<div class="app-container">
+			<h1 class="title">Dark theme</h1>
+			<button class="button">A button</button>
+			</div>
+		</div>
 	</body>
 </html>
 
